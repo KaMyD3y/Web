0ac418df13224f013247b63f400b2d7d5c75590a
Mihail <mihai.kamydzy@gmail.com>
2024-05-25 13:09:05 +0300

    update

---
+++ Homework.js/Proprietatilewindow.js
@@ -0,0 +1,12 @@
+console.log(window.caches);//ofera acces la cache storage api , care permite aplicatillor web sa stocheze in cache resurse pentru a le utiliza offline!
+console.log(window.closed);//indica daca fereastra a fost inchisa!
+console.log(window.console);//ofera acces la consola de dezvoltare a browserului!
+console.log(window.cookieStore);//ofera acces la gestionarea cookie-urilor folosind api(experimental!)
+console.log(window.credentialless);//indica daca fereastra este deschisa cu atributul(experimental!)
+console.log(window.crossOriginIsolated);//indica daca pagina curenta ruleaza intr-un context izolat(cross-origin)
+console.log(window.crypto);//ofera acces la APi-ul web cryptography!
+console.log(window.customElements);//ofera acces la api-ul custom elements pentru definirea elementelor html personalizate!
+console.log(window.devicePixelRatio);//raportul dintre pixeli fizici si pixeli independenti de dispozitiv dip
+console.log(window.document);//reprezinta documentul incarcat in fereastra!
+console.log(window.documentPictureInPicture);//ofera acces la APi-ul document picture-in-picture(experimental!)
+
