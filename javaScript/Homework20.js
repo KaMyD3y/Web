73fc3bf35875ec0d190992f981560b8f87f02641
Mihail <mihai.kamydzy@gmail.com>
2024-06-05 15:57:39 +0300

    update

--- javaScript/Homework20.js
+++ javaScript/Homework20.js
@@ -1,8 +1,4 @@
 let age = 20;
 const status = age >= 18 ? 'adult' : 'minor';
 console.log(status)
-let check = document.getElementById('check').value;
-let minus = check - 13;
-let checking = minus === 13;
-let suma = 13*2;
-document.getElementById('result').innerText = 'Resultatul daca suma este 13' + checking;
+
