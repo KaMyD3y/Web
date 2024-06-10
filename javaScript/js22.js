e90df33a911f91dd90a4e794560458f038622a3d
Mihail <mihai.kamydzy@gmail.com>
2024-06-10 15:09:01 +0300

    update

---
+++ javaScript/js22.js
@@ -0,0 +1,17 @@
+let numbers = [ 10, 20, 30, 40, 80]
+console.log(numbers)
+console.log(numbers.length)
+console.log(numbers[2])
+numbers[3] = 50
+console.log(numbers.length)
+console.log(numbers)
+numbers.length = 7
+console.log(numbers.length)
+console.log(numbers)
+document.getElementById('result').innerText = numbers
+const firstArray = [ 1, 2, 3, 4]
+console.log(firstArray)
+const secondArray = [ 'a', 'b' , 'c', 'd']
+console.log(secondArray)
+const newArray = firstArray.concat(secondArray)
+console.log(newArray)
